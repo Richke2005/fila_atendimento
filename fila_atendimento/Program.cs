f39a8a1d3c34a73185e1c1f5c94cb1a438a8b800
unknown <richardke18@gmail.com>
2022-05-24 17:11:57 -0300

    arquivos de projetos adicionados

---
+++ fila_atendimento/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fila_atendimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
